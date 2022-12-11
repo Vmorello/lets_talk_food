e24c32c50427a830ada72e4b289dc31606815a29
victoriom <Lumv&5332>
2022-12-11 14:38:51 -0500

    typescript upgrade canvas project & component reuseable with the util changing.

---
+++ src/canvas/component.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+import {CanvasControl} from './utils';
+
+interface canvaProps {
+  width:number
+  height:number
+  offsetX?:number
+  offtsetY?:number
+}
+
+type cancasStateType = {
+  ref:React.RefObject<HTMLCanvasElement>
+  util?:CanvasControl
+}
+
+export function CanvasComp(props:canvaProps){
+
+  const refreshRate = 20
+
+  const canvas_ref = React.createRef<HTMLCanvasElement>()
+  const cancasState: cancasStateType = {
+    ref: canvas_ref,
+    util: undefined 
+    }
+  const [canvas,setCanvas] = useState(cancasState);
+    
+  // This function happen once the component is mounted the first time
+  useEffect(()=>{
+    const canvas_current = canvas_ref.current
+    
+    const canvas_util = new CanvasControl(canvas_current!)
+
+    setCanvas({
+      ref: canvas_ref,
+      util: canvas_util
+    })
+  }, [])
+
+  // This function happen every time the component is updated
+  useEffect(()=>{
+    if (canvas.util === undefined) 
+      {return} // Makes this safe to do canvas-util operations
+
+    canvas.util.setup(props)
+    
+    setTimeout(()=> {
+      canvas.util!.startAnimation(props)()
+    }, refreshRate);
+  })
+
+
+  return(
+      <canvas ref={canvas.ref} //onClick={props.onPress()} 
+          width={props.width} height={props.height}
+          style={{border:"3px dotted #000000"}}/>
+  )
+}
---
+++ src/canvas/utils.ts
@@ -0,0 +1,48 @@
+interface canvasUtilBase {
+  canvas:HTMLCanvasElement
+  ctx:CanvasRenderingContext2D
+  setup(props:any): void
+  startAnimation(props:any): (()=> void)
+}
+
+interface setupBase {
+}
+
+interface startAnimationBase{
+}
+
+
+
+export class CanvasControl implements canvasUtilBase{
+  canvas:HTMLCanvasElement
+  ctx:CanvasRenderingContext2D
+  offset:{
+    x: number;
+    y: number;
+  }
+
+
+
+    constructor(canvas:HTMLCanvasElement, offset = { x: 0, y: 0 }) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext("2d")!;
+      this.offset = offset;
+    }
+
+    setup(props:setupBase){
+      // this.clear()
+    }
+
+    startAnimation(props:startAnimationBase){
+
+      return ()=> {}
+
+    }
+
+
+    clear(clearColor = "#FFFFFF") {
+      //console.log(`trying to clear the background with ${clearColor}`);
+      this.ctx.fillStyle = clearColor;
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
